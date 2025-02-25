e95533e6232b01998f5514c830ab35c11575248b
bhadoriyaa03 <bhadoriyaapriya@gmail.com>
2025-02-25 21:46:45 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,5 @@
+first_name =str(input("Enter your first name:"))
+last_name=str(input("Enter your last name:"))
+
+mssg="Hello, "+first_name+" "+last_name+"! Welcome to the Python program."
+print(mssg)
