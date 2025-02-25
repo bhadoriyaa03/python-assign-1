aa684b855e7f79ce0db0d50274fc4a7560ace52e
bhadoriyaa03 <bhadoriyaapriya@gmail.com>
2025-02-25 21:24:12 +0530

    Create task 1.py

---
+++ task 1.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+print("Addition: ",add)
+print("Substraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
